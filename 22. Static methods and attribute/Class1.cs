ff7acd224f84a33f384659227cfdc315abe91f79
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-11 09:27:31 +0200

    Add project files.

---
+++ 22. Static methods and attribute/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_methods_and_attribute
+{
+    static internal class UseFullTools
+    {
+        public static void SayHi(string name)
+        { 
+           Console.WriteLine("Hi" +name);
+        }
+
+    }
+}
---
+++ 22. Static methods and attribute/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_methods_and_attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+            UseFullTools.SayHi("Evans");
+            Console.ReadLine();
+        }
+    }
+}
